a974873e3f051f025a1525c85cec65e91b235b00
Doug Franklin <doug@dougfranklin.com>
2020-12-03 06:11:31 -0500

    Oops, put it in the notes, but not main.js

--- cmp-adv-01-starting-setup/src/main.js
+++ cmp-adv-01-starting-setup/src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App       from './App.vue';
 import BaseBadge from './components/BaseBadge.vue';
+import BaseCard  from './components/BaseCard.vue';
 
 const app = createApp(App);
 
 app.component('base-badge', BaseBadge); // Used in BadgeList and UserInfo, keep Global
+app.component('base-card', BaseCard);
 
 app.mount('#app');
