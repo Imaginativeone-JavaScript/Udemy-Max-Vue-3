f2341ca642808ef3fa99ea76c695cc1e2b3b5e66
Doug Franklin <doug@dougfranklin.com>
2020-12-24 10:10:35 -0500

    More Router Setups

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -2,12 +2,18 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router'; // 1, 5
 
 import App from './App.vue';
+import TeamsList from './components/teams/TeamsList.vue'; // 7
 
-const router = createRouter({   // 2
-  history: createWebHistory(),  // 4
-  routes: []                    // 3
+const router = createRouter({     // 2
+  history: createWebHistory(),    // 4
+  routes: [                       // 3
+    { path: '/teams', component: TeamsList }, // 6, 8
+    { path: '/users', component: UsersList } // 9
+  ]
 });
 
 const app = createApp(App)
+
+app.use(router); // 10
 
 app.mount('#app');
