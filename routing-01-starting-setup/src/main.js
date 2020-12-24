b7e3349d89ae919e4d50ba0ac8e0060c1dcb40c1
Doug Franklin <doug@dougfranklin.com>
2020-12-24 09:54:46 -0500

    Basic Setup for Routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router'; // 1, 5
 
 import App from './App.vue';
+
+const router = createRouter({   // 2
+  history: createWebHistory(),  // 4
+  routes: []                    // 3
+});
 
 const app = createApp(App)
 
