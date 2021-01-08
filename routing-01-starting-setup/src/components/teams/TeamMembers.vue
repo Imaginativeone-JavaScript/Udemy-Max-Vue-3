fa9c42e189c7239414c3fc7df1a067c089332ea2
Doug Franklin <doug@dougfranklin.com>
2021-01-08 17:30:50 -0500

    Basic Dynamic Routes

--- routing-01-starting-setup/src/components/teams/TeamMembers.vue
+++ routing-01-starting-setup/src/components/teams/TeamMembers.vue
@@ -16,18 +16,35 @@
 import UserItem from '../users/UserItem.vue';
 
 export default {
+  inject: ['users', 'teams'],
   components: {
     UserItem
   },
   data() {
     return {
-      teamName: 'Test',
-      members: [
-        { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
-        { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
-      ],
+      // teamName: 'Test',
+      // members: [
+      //   { id: 'u1', fullName: 'Max Schwarz', role: 'Engineer' },
+      //   { id: 'u2', fullName: 'Max Schwarz', role: 'Engineer' },
+      // ],
+      teamName: '',
+      members: []
     };
   },
+  created() {
+    // Access to injected data, router data in particular
+    const teamId = this.$route.params.teamId // available bc this component in loaded thru the router
+    const selectedTeam = this.teams.find(team => team.id === teamId);
+    const members = selectedTeam.members;
+    const selectedMembers = [];
+
+    for (const member of members) {
+      const selectedUser = this.users.find(user => user.id === member);
+      selectedMembers.push(selectedUser);
+    }
+    this.members = selectedMembers;
+    this.teamName = selectedTeam.name;
+  }
 };
 </script>
 
--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -4,12 +4,14 @@ import { createRouter, createWebHistory } from 'vue-router'; // 1, 5
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue'; // 7
 import UsersList from './components/users/UsersList.vue'; // 10
+import TeamMembers from './components/teams/TeamMembers.vue';
 
 const router = createRouter({     // 2
   history: createWebHistory(),    // 4
   routes: [                       // 3
     { path: '/teams', component: TeamsList }, // 6, 8
-    { path: '/users', component: UsersList } // 9
+    { path: '/users', component: UsersList }, // 9
+    { path: '/teams/:teamId', component: TeamMembers } // order matters /teams/new needs to be abv
   ],
   linkActiveClass: 'active' // router-link-active is the default, update styling
 });
