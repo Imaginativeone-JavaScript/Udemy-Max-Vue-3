d550a87b99ac151f9b2a7c04f0d6b4b1a59cb7bf
Doug Franklin <doug@dougfranklin.com>
2021-01-20 07:43:47 -0500

    First use of the  variable

--- vuex-01-starting-setup/src/main.js
+++ vuex-01-starting-setup/src/main.js
@@ -1,7 +1,20 @@
 import { createApp } from 'vue';
+import { createStore } from 'vuex'; // One store per app
 
 import App from './App.vue';
 
+// $store; state
+const store = createStore({
+
+  state() {
+    return {
+      counter: 0
+    }; // a bit like data() in a component
+  }
+
+});
+
 const app = createApp(App);
 
+app.use(store);
 app.mount('#app');
